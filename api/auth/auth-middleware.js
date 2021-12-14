876109754bba374e3dc9eb624e932985e150b3c8
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-12-14 11:32:17 -0500

    restricted middleware completed

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,24 +1,29 @@
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../secrets'); // use this secret!
 
 const restricted = (req, res, next) => {
-  /*
-    If the user does not provide a token in the Authorization header:
-    status 401
-    {
-      "message": "Token required"
-    }
+  const token = req.headers.authorization;
+  if (!token) {
+    next({
+      status: 401,
+      message: 'Token required',
+    });
+  } else {
+    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        next({
+          status: 401,
+          message: 'Token invalid',
+        });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  }
+};
 
-    If the provided token does not verify:
-    status 401
-    {
-      "message": "Token invalid"
-    }
-
-    Put the decoded token in the req object, to make life easier for middlewares downstream!
-  */
-}
-
-const only = role_name => (req, res, next) => {
+const only = (role_name) => (req, res, next) => {
   /*
     If the user does not provide a token in the Authorization header with a role_name
     inside its payload matching the role_name passed to this function as its argument:
@@ -29,8 +34,7 @@ const only = role_name => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
-}
-
+};
 
 const checkUsernameExists = (req, res, next) => {
   /*
@@ -40,8 +44,7 @@ const checkUsernameExists = (req, res, next) => {
       "message": "Invalid credentials"
     }
   */
-}
-
+};
 
 const validateRoleName = (req, res, next) => {
   /*
@@ -62,11 +65,11 @@ const validateRoleName = (req, res, next) => {
       "message": "Role name can not be longer than 32 chars"
     }
   */
-}
+};
 
 module.exports = {
   restricted,
   checkUsernameExists,
   validateRoleName,
   only,
-}
+};
